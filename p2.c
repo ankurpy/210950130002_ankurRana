ac0f45407f2ebac753ad62b66ea57471a48e2423
ankurpy <ankurcpp@gmail.com>
2021-12-13 23:06:06 -0800

    submission

--- p2.c
+++ p2.c
@@ -10,7 +10,7 @@ int main()
 {
     char buff[16];
     int fd;
-    fd = open("desdfifo",O_RDONLY);
+    fd = open("demofifo",O_RDONLY);
     read(fd,buff,16);
     printf("read: %s",buff);
     close(fd);
